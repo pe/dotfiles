a75df650e65f636cefa84bd2d92e250e6354d5a1
Philippe Eberli <p.eberli@gmx.ch>
2021-12-14 08:33:30 +0100

    remove thefuck

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,3 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 
 set PATH $HOME/.jenv/bin $PATH
 status --is-interactive; and source (jenv init -|psub)
-
-thefuck --alias | source
