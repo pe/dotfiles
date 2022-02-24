0ab516d2af8b0582f072be8b8f1c74386aae77fe
Philippe Eberli <p.eberli@gmx.ch>
2022-02-24 22:18:21 +0100

    Remove git column from exa

--- .config/fish/functions/exa.fish
+++ .config/fish/functions/exa.fish
@@ -1,3 +1,3 @@
 function exa --description 'alias exa=exa --icons --group-directories-first'
- command exa --icons --group-directories-first --git $argv; 
+ command exa --icons --group-directories-first $argv; 
 end
