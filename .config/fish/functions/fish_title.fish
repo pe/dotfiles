e8c5537e67bc71d163778af6b0708701e5ee46f0
Philippe Eberli <p.eberli@gmx.ch>
2022-03-12 23:17:26 +0100

    Fish: use prompt_pwd for working directory instead of fish_pwd

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,18 +1,14 @@
 function fish_title
     # emacs' "term" is basically the only term that can't handle it.
     if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
-        # replace $HOME with ~
-        set -l working_directory (__fish_pwd | string replace $HOME '~')
-
         # An override for the current command is passed as the first parameter.
         # This is used by `fg` to show the true process name, among others.
         set -l command (set -q argv[1] && echo $argv[1] || status current-command)
 
-        if [ "$command" = fish ]
-            # 'fish' is not of interest as a command
-            echo "$working_directory"
-        else
-            echo "$command · $working_directory"
+        # Don't print "fish" because it's redundant
+        if test "$command" = fish
+            set command
         end
+        echo "$command · " (prompt_pwd --full-length-dirs=4 --dir-length=2)
     end
 end
