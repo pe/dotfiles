985003e5c7a98e3965e123e2ec6d0793bbd71d6e
Philippe Eberli <p.eberli@gmx.ch>
2022-09-05 22:52:18 +0200

    Only execute jenv/starship if installed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 /opt/homebrew/bin/brew shellenv | source
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
 set PATH $HOME/.jenv/bin $PATH
 
 if status --is-interactive
-    source (jenv init -|psub)
-    starship init fish | source
+    command -q jenv; and jenv init - | source
+    command -q starship; and starship init fish | source
 end
