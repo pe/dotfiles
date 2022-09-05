09fcb81345b337df8028256fb6648c67d9071f41
Philippe Eberli <p.eberli@gmx.ch>
2022-09-05 22:43:27 +0200

    Only initialize starship in interactive shells

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 set PATH $HOME/.jenv/bin $PATH
-status --is-interactive; and source (jenv init -|psub)
 
-starship init fish | source
+if status --is-interactive
+    source (jenv init -|psub)
+    starship init fish | source
+end
