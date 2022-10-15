b15efd1bfb3e574b8701fb22be1ef1ad83bbbf86
Philippe Eberli <p.eberli@gmx.ch>
2022-10-15 21:18:13 +0200

    Iterm shell integrations after starship. They won't work otherwise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,9 @@
 /opt/homebrew/bin/brew shellenv | source
 
-test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
-
 set PATH $HOME/.jenv/bin $PATH
 
 if status --is-interactive
     command -q jenv; and jenv init - | source
     command -q starship; and starship init fish | source
+    test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 end
