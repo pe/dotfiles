6b1fa8bb55add41680e4c8a1174047eeb5e0c61c
Philippe Eberli <philippe.eberli@adcubum.com>
2022-10-18 09:01:02 +0200

    oc completion for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,6 @@ set PATH $HOME/.jenv/bin $PATH
 if status --is-interactive
     command -q jenv; and jenv init - | source
     command -q starship; and starship init fish | source
+    command -q oc; and oc completion fish | source
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 end
