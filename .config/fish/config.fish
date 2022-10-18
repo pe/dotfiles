2af5a27301c78afb0c09a7338671cd2857aab16d
Philippe Eberli <philippe.eberli@adcubum.com>
2022-10-18 16:42:19 +0200

    Move yadm & openshift from prompt to the iTerm statusbar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,12 @@ if status --is-interactive
     command -q starship; and starship init fish | source
     command -q oc; and oc completion fish | source
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+    function iterm2_print_user_vars
+        iterm2_set_user_var openshiftProject $(command oc project -q)
+        function yadm_status
+            echo (command yadm --no-optional-locks status --untracked-files=no --branch --porcelain | string match --regex '## .+ \[(.+)\]')[2]
+            echo (command yadm --no-optional-locks diff --shortstat HEAD)
+        end
+        iterm2_set_user_var yadmDirty $(yadm_status)
+    end
 end
