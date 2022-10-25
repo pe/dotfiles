9d7beefe162c6ad2df760ca5550e26bde5ce2ac1
Philippe Eberli <philippe.eberli@adcubum.com>
2022-10-25 09:09:05 +0200

    Supress error when not logged in openshift

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ if status --is-interactive
     command -q oc; and oc completion fish | source
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
     function iterm2_print_user_vars
-        iterm2_set_user_var openshiftProject $(command oc project -q)
+        iterm2_set_user_var openshiftProject $(command oc project -q 2>&1)
         function yadm_status
             echo (command yadm --no-optional-locks status --untracked-files=no --branch --porcelain | string match --regex '## .+ \[(.+)\]')[2]
             echo (command yadm --no-optional-locks diff --shortstat HEAD)
