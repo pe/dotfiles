33c90d49751327de52ba51ea62dee6c233c28ed8
Philippe Eberli <p.eberli@gmx.ch>
2022-10-31 22:57:37 +0100

    Dirty fix for iterm / starship incobatibility

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set PATH $HOME/.jenv/bin $PATH
 if status --is-interactive
     command -q jenv; and jenv init - | source
     command -q starship; and starship init fish | source
+    set -e STARSHIP_SHELL # Breaks iterm shell integration
     command -q oc; and oc completion fish | source
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 end
