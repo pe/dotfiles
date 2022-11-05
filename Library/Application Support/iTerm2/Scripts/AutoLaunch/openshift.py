b31a9f60e748dccdbfb1be4f788b5d0cee4842c7
Philippe Eberli <p.eberli@gmx.ch>
2022-11-05 12:42:38 +0100

    Update openshift/yadm scripts

--- Library/Application Support/iTerm2/Scripts/AutoLaunch/openshift.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/openshift.py
@@ -1,55 +1,58 @@
 #!/usr/bin/env python3.7
 
 import asyncio
+from asyncio.subprocess import PIPE
 import textwrap
+import time
 import iterm2
-# This script was created with the "basic" environment which does not support adding dependencies
-# with pip.
 
-COUNTER = 0
-#return code for command not found
-EX_NOTFOUND = 127
+EX_NOTFOUND = 127 #return code for command not found
 OPENSHIFT_COMMANDS = ["oc login ", "oc logout", "oc project "]
+ICON_1X = iterm2.StatusBarComponent.Icon(1, "iVBORw0KGgoAAAANSUhEUgAAABAAAAARCAQAAAB+puRPAAAA0ElEQVR4Aa3BTyuDARwA4KdNqSW77cDB1U0KFyXlGyw77CgHR7XkpFyEWiFuXFzWysVJSZx9hZ3sMpTyp1yWpZ/a1l5r1z2PIZuxrqxk3ogBk6pCCCHULOmT0xB+3Tpz7V34sWpBV8qDUDerY9yRcy13RrUtCy1zEtP2rek5EG6kJSpCVU/Zm087EhuanlV0pR0qykhkbQuP2la8qEv5L6Mm7Gmb0BSO7crpGHMlfJnSlVfwLXy4tOVEQwh5fRY9CSGE8KpoQEbBqXsXNmUNzR+ScESlOaPFVAAAAABJRU5ErkJggg==")
+ICON_2X = iterm2.StatusBarComponent.Icon(2, "iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAYAAAA+stv/AAAB0ElEQVR4Ae3SA6xcQRSA4dq2bdtmVNt2g9qNatu2bdu2bbtdnv4b3sxy7r4X3j/5Ysycc6JYWZksO5qjBwahAyojMSKtlBiNZxA/bNiGuoiwomE4fkM07ERGhFVyHIKY9AONYKqo2AcJ4ickABtqQrvBEKjeoidKIwk8ZUZLLIANoviBNliEgQhaeTggBk5MRyIEKjeuQ3Ab/+CEQGALdhuJ8BKi6I1QS4Qi8NQfgo+YhTIIWBuIYgvMFAP1sAAxEVKb1Z0jCXQqiOn4AMEjhFQc/IIYjIduAyGK0ghaDoiiBlJDpzRwmLmhohAI3HDiIn5At7sQg7Xoi0nwWznswEq4IRC4EBM6ncQ3vMI/uCHYhKDVhuAuhiATdCuOODgLMViKgCVFP3RHuCXBX4jBcPisAbbABsE8hFtfiKIgfHYMYvABCWC2Qurv8Rh+awtRrICnGMiMUIuPuxDFIPgtNp5BDGzYiA/4i9YIVklcgigOIxoC1hKvMAYHIVAdREfkVo6tFObCBVG8RxoELSqiw1NCXIYE8BHvIQG4UBOmSoXrEJNs6IawioWRsEE03EcJRFj5sBhfIX64cQktER2RUixUQ1eMxWgMQFOkglZWVv8BVWcaDO4vw3sAAAAASUVORK5CYII=")
+
+async def calculate():
+    """Returns the current active openshift project"""
+    proc = await asyncio.create_subprocess_shell("oc project -q", stdout=PIPE, stderr=PIPE)
+    stdout, stderr = await proc.communicate()
+    if proc.returncode == EX_NOTFOUND:
+        return ''
+    if stderr:
+        stderr_decoded = stderr.decode()
+        if "(Unauthorized)" in stderr_decoded:
+            return 'Not logged in'
+        return [f'{textwrap.shorten(stderr_decoded, width=width, placeholder="…")}'
+                for width in [20, 40, 60, 80, 100, 120]]
+
+    return f'{stdout.decode().strip()}'
 
 async def main(connection):
-    icon2x = iterm2.StatusBarComponent.Icon(2, "iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAYAAAA+stv/AAAB0ElEQVR4Ae3SA6xcQRSA4dq2bdtmVNt2g9qNatu2bdu2bbtdnv4b3sxy7r4X3j/5Ysycc6JYWZksO5qjBwahAyojMSKtlBiNZxA/bNiGuoiwomE4fkM07ERGhFVyHIKY9AONYKqo2AcJ4ickABtqQrvBEKjeoidKIwk8ZUZLLIANoviBNliEgQhaeTggBk5MRyIEKjeuQ3Ab/+CEQGALdhuJ8BKi6I1QS4Qi8NQfgo+YhTIIWBuIYgvMFAP1sAAxEVKb1Z0jCXQqiOn4AMEjhFQc/IIYjIduAyGK0ghaDoiiBlJDpzRwmLmhohAI3HDiIn5At7sQg7Xoi0nwWznswEq4IRC4EBM6ncQ3vMI/uCHYhKDVhuAuhiATdCuOODgLMViKgCVFP3RHuCXBX4jBcPisAbbABsE8hFtfiKIgfHYMYvABCWC2Qurv8Rh+awtRrICnGMiMUIuPuxDFIPgtNp5BDGzYiA/4i9YIVklcgigOIxoC1hKvMAYHIVAdREfkVo6tFObCBVG8RxoELSqiw1NCXIYE8BHvIQG4UBOmSoXrEJNs6IawioWRsEE03EcJRFj5sBhfIX64cQktER2RUixUQ1eMxWgMQFOkglZWVv8BVWcaDO4vw3sAAAAASUVORK5CYII=")
-    icon1x = iterm2.StatusBarComponent.Icon(1, "iVBORw0KGgoAAAANSUhEUgAAABAAAAARCAQAAAB+puRPAAAA0ElEQVR4Aa3BTyuDARwA4KdNqSW77cDB1U0KFyXlGyw77CgHR7XkpFyEWiFuXFzWysVJSZx9hZ3sMpTyp1yWpZ/a1l5r1z2PIZuxrqxk3ogBk6pCCCHULOmT0xB+3Tpz7V34sWpBV8qDUDerY9yRcy13RrUtCy1zEtP2rek5EG6kJSpCVU/Zm087EhuanlV0pR0qykhkbQuP2la8qEv5L6Mm7Gmb0BSO7crpGHMlfJnSlVfwLXy4tOVEQwh5fRY9CSGE8KpoQEbBqXsXNmUNzR+ScESlOaPFVAAAAABJRU5ErkJggg==")
-
-    app = await iterm2.async_get_app(connection)
-
-    # Register the status bar component.
     component = iterm2.StatusBarComponent(
         short_description="openshift",
-        detailed_description="The currently configured project for openshift",
+        detailed_description="The current active openshift project",
         exemplar="projectname",
-        icons=[icon1x,icon2x],
-        update_cadence=600,
+        icons=[ICON_1X, ICON_2X],
+        update_cadence=None,
         identifier="com.openshift",
         knobs=[])
 
+    app = await iterm2.async_get_app(connection)
+
+    async def update():
+        """A background tasks that periodically updates the status bar."""
+        while True:
+            print("Updating")
+            await app.async_set_variable("user.openshift", await calculate())
+            await asyncio.sleep(10 * 60) #10min
+    asyncio.create_task(update())
+
     @iterm2.StatusBarRPC
-    async def openshift_coroutine(knobs,
-        _=iterm2.Reference("user.openshiftExecuted?"),):
-        proc = await asyncio.create_subprocess_shell(
-            "oc project -q",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await proc.communicate()
-        if proc.returncode == EX_NOTFOUND:
-            return ''
-        if "(Unauthorized)" in stderr.decode():
-            return 'Not logged in'
-        if stderr:
-            return [f'{textwrap.shorten(stderr.decode(), width=width, placeholder="…")}'
-                    for width in [20, 40, 60, 80, 100, 120]]
-
-        return f'{stdout.decode().strip()}'
-
-    await component.async_register(connection, openshift_coroutine)
+    async def coroutine(knobs, value=iterm2.Reference("iterm2.user.openshift?"),):
+        return value if value else ""
+    await component.async_register(connection, coroutine)
 
     async def monitor(session_id):
-        """Check for openshift commands and trigger a update"""
+        """Check for oc commands and update the status bar."""
         session = app.get_session_by_id(session_id)
         if not session:
             return
@@ -58,11 +61,8 @@ async def main(connection):
             while True:
                 _, command = await mon.async_get()
                 if any(openshift_command in command for openshift_command in OPENSHIFT_COMMANDS):
-                    global COUNTER
-                    COUNTER += 1
                     await asyncio.sleep(1)
-                    await session.async_set_variable("user.openshiftExecuted", COUNTER)
-
+                    await app.async_set_variable("user.openshift", time.time())
     await iterm2.EachSessionOnceMonitor.async_foreach_session_create_task(app, monitor)
 
 iterm2.run_forever(main)
--- Library/Application Support/iTerm2/Scripts/AutoLaunch/yadm.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/yadm.py
@@ -1,77 +1,76 @@
 #!/usr/bin/env python3.7
 
 import asyncio
+from asyncio.subprocess import PIPE
 import re
 import os
 import iterm2
-# This script was created with the "basic" environment which does not support adding dependencies
-# with pip.
 
-COUNTER = 0
 BRANCH = re.compile(r"## .+ \[(.+)\]")
+ICON_1X = iterm2.StatusBarComponent.Icon(1, "iVBORw0KGgoAAAANSUhEUgAAABAAAAARCAQAAAB+puRPAAAAhElEQVR42mNgoBbgYNCAQz5sCnYwrIPCVwxPGBwwFTyEs/YweDBcYvDAp0CDQZLhLIMLbgV3Ga4zPGFYjksBCwMrAyuDA8MKXAogwAaXAgeGTgYt3ApYGd4y/GfYj8+Emwz/wc7DqYCHwY6BFZ8CnI5cxnARBd5kCENVwMwgjAIFiI5mAAATLDH7OeagAAAAAElFTkSuQmCC")
+ICON_2X = iterm2.StatusBarComponent.Icon(2, "iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAQAAACUuxN0AAAA9UlEQVR42mNgGAXoQJ3hGsMLLPAJw3QGYcLadRheMLxjeIwGXzH8Z/jH8J/hAmEjGhj+MyRjiFow/GfYwbCeGCNwG7CZgY1hM2Ej8BnAwMDOsJ2QEfgNYGDgYNiN3whCBjAwcDHsZ/jPsI10Ay4xZEBhCcNXhv8MCqQagA6NSTFAjCEZBV4h1QB0sIOuBogyuDOYM7CQa0AgwxdwkJ1lECTHABaGN/BQ7yLHAGWkaNtLjgH8SAYsJy8MpkC1f2MwJzcWtBhaGJIY+AYwHYw8A4IY/jEcZphDAD5heMUgicv0TCyFBzp8xaCDz4E2DP4EoNIwq40BPdXJrReYsrUAAAAASUVORK5CYII=")
 
-async def fetch():
-    """A background tasks that periodically fetches the remote of the yadm repo"""
-    while True:
-        proc = await asyncio.create_subprocess_shell(
-            "yadm fetch --quiet",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
-        stdout, stderr = await proc.communicate()
-        print("yadm fetch")
-        if stdout:
-            print(stdout.decode())
-        if stderr:
-            print(stderr.decode())
-        await asyncio.sleep(12 * 60 * 60) #12h
+async def calculate():
+    """The status of a yadm managed home."""
+    command = "yadm --no-optional-locks status --untracked-files=no --branch --porcelain"
+    proc = await asyncio.create_subprocess_shell(command, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = await proc.communicate()
+    if stderr:
+        return stderr.decode().strip()
+
+    lines = stdout.decode().splitlines()
+    match = re.match(BRANCH, lines[0])
+    ahead_behind = f'{match.group(1)} commits' if match else ""
+    paths = [line[3:].strip(' "') for line in lines[1:]]
+    filenames = [os.path.basename(path) for path in paths]
+    file_count = f"{len(paths)} files changed" if paths else ""
+    if paths or ahead_behind:
+        return [" – ".join(filter(None, [ahead_behind, " ".join(paths)])),
+                " – ".join(filter(None, [ahead_behind, " ".join(filenames)])),
+                " – ".join(filter(None, [ahead_behind, file_count]))]
+    return "✓"
 
 async def main(connection):
-    icon2x = iterm2.StatusBarComponent.Icon(2, "iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAQAAACUuxN0AAAA9UlEQVR42mNgGAXoQJ3hGsMLLPAJw3QGYcLadRheMLxjeIwGXzH8Z/jH8J/hAmEjGhj+MyRjiFow/GfYwbCeGCNwG7CZgY1hM2Ej8BnAwMDOsJ2QEfgNYGDgYNiN3whCBjAwcDHsZ/jPsI10Ay4xZEBhCcNXhv8MCqQagA6NSTFAjCEZBV4h1QB0sIOuBogyuDOYM7CQa0AgwxdwkJ1lECTHABaGN/BQ7yLHAGWkaNtLjgH8SAYsJy8MpkC1f2MwJzcWtBhaGJIY+AYwHYw8A4IY/jEcZphDAD5heMUgicv0TCyFBzp8xaCDz4E2DP4EoNIwq40BPdXJrReYsrUAAAAASUVORK5CYII=")
-    icon1x = iterm2.StatusBarComponent.Icon(1, "iVBORw0KGgoAAAANSUhEUgAAABAAAAARCAQAAAB+puRPAAAAhElEQVR42mNgoBbgYNCAQz5sCnYwrIPCVwxPGBwwFTyEs/YweDBcYvDAp0CDQZLhLIMLbgV3Ga4zPGFYjksBCwMrAyuDA8MKXAogwAaXAgeGTgYt3ApYGd4y/GfYj8+Emwz/wc7DqYCHwY6BFZ8CnI5cxnARBd5kCENVwMwgjAIFiI5mAAATLDH7OeagAAAAAElFTkSuQmCC")
-
-    # periodically fetch the remote of the yadm repo
-    asyncio.create_task(fetch())
-
-    app = await iterm2.async_get_app(connection)
-
-    # Register the status bar component.
     component = iterm2.StatusBarComponent(
         short_description="yadm",
-        detailed_description="Shows if there are changes to a yadm managed home directory",
+        detailed_description="The status of a yadm managed home.",
         exemplar="1 ahead commits – 2 files changed",
-        icons=[icon1x,icon2x],
-        update_cadence=600,
+        icons=[ICON_1X, ICON_2X],
+        update_cadence=None,
         identifier="io.yadm",
         knobs=[])
 
+    app = await iterm2.async_get_app(connection)
+
+    async def fetch():
+        """A background task that periodically fetches the yadm repo."""
+        while True:
+            command = "yadm fetch --quiet"
+            proc = await asyncio.create_subprocess_shell(command, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = await proc.communicate()
+            print("yadm fetch")
+            if stdout:
+                print(stdout.decode())
+            if stderr:
+                print(stderr.decode())
+            await asyncio.sleep(12 * 60 * 60) #12h
+    asyncio.create_task(fetch())
+
+    async def update():
+        """A background tasks that periodically updates the status bar."""
+        while True:
+            print("Updating")
+            await app.async_set_variable("user.yadm", await calculate())
+            await asyncio.sleep(10 * 60) #10min
+    asyncio.create_task(update())
+
     @iterm2.StatusBarRPC
-    async def yadm_coroutine(knobs,
-            _=iterm2.Reference("user.yadmExecuted?"),):
-        proc = await asyncio.create_subprocess_shell(
-            "yadm --no-optional-locks status --untracked-files=no --branch --porcelain",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await proc.communicate()
-        if stderr:
-            return stderr.decode().strip()
-
-        lines = stdout.decode().splitlines()
-        match = re.match(BRANCH, lines[0])
-        ahead_behind = f'{match.group(1)} commits' if match else ""
-        paths = [line[3:].strip(' "') for line in lines[1:]]
-        filenames = [os.path.basename(path) for path in paths]
-        file_count = f"{len(paths)} files changed" if paths else ""
-        if paths or ahead_behind:
-            return [" – ".join(filter(None, [ahead_behind, " ".join(paths)])),
-                    " – ".join(filter(None, [ahead_behind, " ".join(filenames)])),
-                    " – ".join(filter(None, [ahead_behind, file_count]))]
-        return "clean"
-
-    await component.async_register(connection, yadm_coroutine)
+    async def coroutine(knobs, value=iterm2.Reference("iterm2.user.yadm?")):
+        return value if value else ""
+    await component.async_register(connection, coroutine)
 
     async def monitor(session_id):
-        """Check for yadm commands and trigger a update"""
+        """Listen for yadm commands and trigger an update"""
         session = app.get_session_by_id(session_id)
         if not session:
             return
@@ -80,10 +79,9 @@ async def main(connection):
             while True:
                 _, command = await mon.async_get()
                 if "yadm " in command:
-                    global COUNTER
-                    COUNTER += 1
-                    await session.async_set_variable("user.yadmExecuted", COUNTER)
-
+                    await asyncio.sleep(1)
+                    await app.async_set_variable("user.yadm", await calculate())
+                    print("Var set from command")
     await iterm2.EachSessionOnceMonitor.async_foreach_session_create_task(app, monitor)
 
 iterm2.run_forever(main)
