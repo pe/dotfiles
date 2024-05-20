5b60bbcd3cfe5d503d506e219ae1025ffd4dc2f9
Philippe Eberli <p.eberli@gmx.ch>
2024-05-20 22:19:50 +0200

    Remove workaround for incompatibility between starship/fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,6 @@ set PATH $HOME/.jenv/bin $PATH
 if status --is-interactive
     command -q jenv; and jenv init - | source
     command -q starship; and starship init fish | source
-    set -e STARSHIP_SHELL # Breaks iterm shell integration
     command -q oc; and oc completion fish | source
     command -q zoxide; and zoxide init fish | source
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
